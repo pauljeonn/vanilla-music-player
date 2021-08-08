5e25425458bbe387bc1e13a5fe118bd1e58aef00
Paul Jeon <pauljeonn@gmail.com>
2021-08-08 23:53:14 +0900

    add event functions

--- script.js
+++ script.js
@@ -23,3 +23,52 @@ function loadSong(song) {
 	audio.src = `music/${song}.mp3`;
 	audio.src = `images/${song}.jpg`;
 }
+
+function playSong() {
+	musicContainer.classList.add('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-play');
+	playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+	audio.play();
+}
+
+function pauseSong() {
+	musicContainer.classList.add('play');
+	playBtn.querySelector('i.fas').classList.remove('fa-pause');
+	playBtn.querySelector('i.fas').classList.add('fa-play');
+
+	audio.pause();
+}
+
+function prevSong() {
+	songIndex--;
+
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+function nextSong() {
+	songIndex++;
+
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+	const isPlaying = musicContainer.classList.contains('play');
+
+	if (isPlaying) {
+		pauseSong();
+	} else {
+		playSong();
+	}
+});
